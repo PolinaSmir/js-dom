285344f012fdb9b50ef7dfbff9850b63e30fe3a3
Polina <smivasilisa@gmail.com>
2024-02-09 18:59:22 +0100

    dsc

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-// const form = document.getElementById("name-form");
+const collection = document.getElementsByClassName("paragraph");
 
-// form.addEventListener("submit", consoleForm);
-
-// function consoleForm(event) {
-//   event.preventDefault();
-
-//   const form = event.target;
-//   const value = form[0].value;
-//   console.dir(value);
+// for (let i = 0; i < collection.length; i++) {
+//   collection[i].style.color = "green";
 // }
 
-const form = document.getElementById("formocka");
-
-form.addEventListener("submit", consoleForm);
-
-function consoleForm(event) {
-  event.preventDefault();
-  const form = event.target;
-  const value = form[0].value;
-  console.dir(`hello ${value}`);
+for (let p of collection) {
+  p.style.color = "green";
 }
+
+const btns = document.getElementsByTagName("button");
+const arr = [...btns];
+//console.log(arr);
+
+arr.forEach((button) => {
+  button.style.color = "red";
+});
+
+function hello(event) {
+  console.log("Hello user!");
+
+  event.target.removeEventListener("click", hello);
+}
+
+for (let btn of btns) {
+  btn.addEventListener("click", hello);
+}
+
+const btn = document.querySelector("button");
+// console.log(btn);
+
+const btn2 = document.querySelector(".super-btn");
+console.log(btn2);
+
+const btn3 = document.querySelector("#btn-btn");
+console.log(btn3);
+
+const btnCollection = document.querySelectorAll("button");
+console.log(btnCollection);
