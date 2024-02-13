c29a6f0567dbd1ebf222c38b5d92d8b9dcd71f42
Polina <smivasilisa@gmail.com>
2024-02-13 18:41:10 +0100

    parentNode +dataset

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-const CURRENCY = {
-  USD_TO_UAH: 37.65,
-  EUR_TO_UAH: 40.61,
-};
+const button = document.querySelectorAll("button");
+const div = document.querySelector("#root");
 
-const converterForm = document.querySelector("#currency-converter-form");
+console.dir(button);
 
-converterForm.addEventListener("submit", convertHandler);
+for (let btn of button) {
+  btn.addEventListener("click", clickHandler);
+}
 
-function convertHandler(event) {
-  event.preventDefault();
+function clickHandler({
+  target: {
+    dataset: { color },
+    parentNode,
+  },
+}) {
+  // const {
+  //   dataset: { color },
+  // } = target;
+  // const { parentNode } = target;
 
-  const amount = Number(document.querySelector("#amount").value);
-  const currency = document.querySelector("#currency").value;
-
-  let convertedAmount;
-
-  const { USD_TO_UAH: usdCourse, EUR_TO_UAH: eurCourse } = CURRENCY;
-
-  switch (currency) {
-    case "USD": {
-      convertedAmount = amount * usdCourse;
-      break;
-    }
-    case "EUR": {
-      convertedAmount = amount * eurCourse;
-      break;
-    }
-    default:
-      throw new Error("Congratulations you broke it");
-  }
-
-  // if (currency === "USD") {
-  //   convertedAmount = amount * usdCourse;
-  // } else if (currency === "EUR") {
-  //   convertedAmount = amount * eurCourse;
-  // } else {
-  //   throw new Error("Congratulations you broke it");
-  // }
-
-  const resultDiv = document.querySelector("#result");
-  resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(
-    2
-  )} UAH`;
+  parentNode.style.backgroundColor = color;
 }
